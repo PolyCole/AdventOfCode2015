265a05898d9ebb19c00e4dd471f329c52af7c999
Cole Polyak <colepolyak@gmail.com>
2018-02-23 22:24:14 -0700

    Initial commit for Day3 code and input.

---
+++ Day3/Day3Pt1.java
@@ -0,0 +1,90 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day3Pt1 {
+
+	public static void main(String[] args) {
+		
+		Scanner file = null;
+		
+		try
+		{
+			file = new Scanner(new FileInputStream("Day3Input.txt"));
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		int[][] arr = new int[10000][10000];
+		
+		int x = 4999;
+		int y = 4999;
+		
+		int count = 0;
+		
+		String input = file.next();
+		System.out.println(input);
+		int currentIndex = 0;
+		
+		for(int i = 0; i < input.length(); ++i)
+		{
+			String cur = input.substring(currentIndex, currentIndex+1);
+			
+			if("<".equals(cur))
+			{
+				System.out.println("<");
+				++arr[x][y];
+				++arr[x-1][y];
+				--x;
+			}
+			
+			else if(">".equals(cur))
+			{
+				System.out.println(">");
+				++arr[x][y];
+				++arr[x+1][y];
+				++x;
+			}
+			else if("V".equals(cur))
+			{
+				System.out.println("V");
+				++arr[x][y];
+				++arr[x][y-1];
+				--y;
+			}
+			else if("^".equals(cur))
+			{
+				System.out.println("^");
+				++arr[x][y];
+				++arr[x][y+1];
+				++y;
+			}
+			
+			++count;
+			++currentIndex;
+		}
+		
+		int housesReceivingAtLeastOne = 0;
+		
+		for(int i = 0; i < arr.length; ++i)
+		{
+			for(int j = 0; j < arr[i].length; ++j)
+			{
+				if(arr[i][j] >= 1)
+				{
+					++housesReceivingAtLeastOne; 
+				}
+			}
+		}
+		
+		System.out.println("Total iterative count: " + count);
+		System.out.println("Houses receiving atleast one gift: " + housesReceivingAtLeastOne);
+		
+		
+
+	}
+
+}
